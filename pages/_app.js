42460463c5b05bac24b4d56bd9b2457105f962f7
Ryan Freeman <N00173767@student.iadt.ie>
2021-11-17 21:47:25 +0000

    Changed global.css file extension

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 
 function App({Component, pageProps}) {
     return <Component {...pageProps} />
