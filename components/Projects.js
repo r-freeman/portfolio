db7745c87f00124d20564328d6b5cfbbb816da3e
Ryan Freeman <N00173767@student.iadt.ie>
2021-11-17 21:49:45 +0000

    Updated page styling at different breakpoints

--- components/Projects.js
+++ components/Projects.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 import ExternalLink from './ExternalLink';
 
 export default function Projects({projectData}) {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 import GitHub from '../components/GitHub';
 import {getCustomData} from '../lib/custom';
 import Projects from '../components/Projects';
