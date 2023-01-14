97f8b7e8bb8a773beb00be600ed9e5952d9d6a9c
r-freeman <hello@ryanfreeman.dev>
2023-01-14 22:30:13 +0000

    Converted generateSitemap to TypeScript

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export function Footer() {
             <OuterContainer>
                 <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
                     <InnerContainer>
-                        <SpotifyPlayer/>
+                        {/*<SpotifyPlayer/>*/}
                         <div className="flex flex-col items-center justify-between gap-6 mt-12">
                             <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                                 <NavLink href="/about">About</NavLink>
--- src/lib/generateSitemap.ts
+++ src/lib/generateSitemap.ts
@@ -5,7 +5,7 @@ import {writeFile} from 'fs/promises'
 
 const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL
 
-async function createSitemap(pages) {
+async function createSitemap(pages: string[]) {
     const sitemap =
         `<?xml version="1.0" encoding="UTF-8"?>
             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
