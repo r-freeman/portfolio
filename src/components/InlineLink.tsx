2ec382d5a3d270c6b8a406f974879e094f5c7c3c
r-freeman <hello@ryanfreeman.dev>
2023-01-16 20:23:17 +0000

    Changed InlineLink style

--- src/components/InlineLink.tsx
+++ src/components/InlineLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function InlineLink({href, children}: { href: string, children: ReactNode }) {
     return (
-        <Link href={href} className="transition hover:text-indigo-500 dark:hover:text-indigo-400">
+        <Link href={href} className="text-indigo-500 dark:text-indigo-400 hover:underline">
             {children}
         </Link>
     )
