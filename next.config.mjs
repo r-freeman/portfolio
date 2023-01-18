2f098f14291361d47621f2df9809c8efc13c0b4f
r-freeman <hello@ryanfreeman.dev>
2023-01-18 20:30:40 +0000

    Updated caching headers for spotify api route

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,19 @@ const nextConfig = {
         } else {
             return []
         }
+    },
+    async headers() {
+        return [
+            {
+                source: '/api/spotify/currently-playing',
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "s-maxage=1, stale-while-revalidate=59"
+                    }
+                ]
+            }
+        ]
     }
 }
 
