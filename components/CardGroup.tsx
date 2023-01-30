5ea298b05a37727dfb030d56419cb473aa14cc8c
r-freeman <hello@ryanfreeman.dev>
2023-01-30 21:26:48 +0000

    Updated CardGroup styling

--- components/CardGroup.tsx
+++ components/CardGroup.tsx
@@ -10,10 +10,10 @@ export function CardGroup({title, children}: CardGroupProps) {
 
     return (
         <section aria-labelledby={id}>
-            <h2 id={id} className="text-sm font-semibold text-zinc-800 dark:text-zinc-100 mb-8">{title}</h2>
+            <h2 id={id} className="text-sm font-semibold text-zinc-800 dark:text-zinc-100">{title}</h2>
             <ul
                 role="list"
-                className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3 mb-16"
+                className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3 mt-8 mb-16"
             >
                 {children}
             </ul>
