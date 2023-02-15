a9d7f5c7cea2a8ba4f75fa49ba9d0f41c38fa6e1
r-freeman <hello@ryanfreeman.dev>
2023-02-15 20:28:09 +0000

    Extracted avatar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import {usePathname} from 'next/navigation'
 import {Fragment, useEffect, useRef} from 'react'
@@ -6,11 +5,11 @@ import {Popover, Transition} from '@headlessui/react'
 import clsx from 'clsx'
 import {Container} from './Container'
 import {MobileNavItem} from './ui/MobileNavItem'
+import {Avatar, AvatarContainer} from './ui/Avatar'
 import {CloseIcon} from './icons/CloseIcon'
 import {ChevronDownIcon} from './icons/ChevronDownIcon'
 import {MoonIcon} from './icons/MoonIcon'
 import {SunIcon} from './icons/SunIcon'
-import avatar from '@/public/static/images/avatar.jpg'
 import type {Props} from 'types'
 
 function MobileNavigation(props: Props) {
@@ -150,40 +149,6 @@ function clamp(num: number, a: number, b: number) {
     let min = Math.min(a, b)
     let max = Math.max(a, b)
     return Math.min(Math.max(num, min), max)
-}
-
-function AvatarContainer({className, ...props}: { style?: Object } & Props) {
-    return (
-        <div
-            className={clsx(
-                className,
-                'h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10'
-            )}
-            {...props}
-        />
-    )
-}
-
-function Avatar({large = false, className, ...props}: { large?: boolean, style?: Object } & Props) {
-    return (
-        <Link
-            href="/"
-            aria-label="Home"
-            className={clsx(className, 'pointer-events-auto')}
-            {...props}
-        >
-            <Image
-                src={avatar}
-                alt=""
-                sizes={large ? '4rem' : '2.25rem'}
-                className={clsx(
-                    'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
-                    large ? 'h-16 w-16' : 'h-9 w-9'
-                )}
-                placeholder="blur"
-            />
-        </Link>
-    )
 }
 
 export function Header() {
