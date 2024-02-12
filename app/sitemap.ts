2e91cca5578ab15e66dc3d6321d93ee6a17add86
Ryan Freeman <hello@ryanfreeman.dev>
2024-02-12 22:58:52 +0000

    Updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const urls = [
         'https://ryanfreeman.dev/',
         'https://ryanfreeman.dev/about',
+        'https://ryanfreeman.dev/reading',
         'https://ryanfreeman.dev/writing',
         'https://ryanfreeman.dev/projects',
         'https://ryanfreeman.dev/uses'
