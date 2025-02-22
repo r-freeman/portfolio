24acc69d20e73c5b3cabac354ac8328fc3e3ec88
Ryan Freeman <hello@ryanfreeman.dev>
2025-02-22 23:24:22 +0000

    Update about page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -64,7 +64,7 @@ function SocialLink({
 
 export default async function About() {
     return (
-        <Container className="mt-16 sm:mt-32">
+        <Container className="mt-36">
             <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
                 <div className="lg:pl-20">
                     <div className="max-w-xs lg:max-w-sm px-2.5">
