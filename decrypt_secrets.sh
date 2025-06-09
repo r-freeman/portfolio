018d10643f0e2d83eb165b7b6383f5a9b4356d8e
Ryan Freeman <hello@ryanfreeman.dev>
2025-06-09 22:31:07 +0100

    Simplify build and publish workflow

--- decrypt_secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output ./.env ./.env.gpg
