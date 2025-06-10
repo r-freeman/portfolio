5e07e1abedcec5089fd41b4ff63fca4da1543887
Ryan Freeman <hello@ryanfreeman.dev>
2025-06-10 14:44:22 +0100

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 cat > docker-compose.prod.yaml <<EOF
+networks:
+  portfolio_net:
+    driver: bridge
+    name: portfolio_net
+
 services:
   portfolio_frontend:
+    container_name: portfolio_frontend
+    image: portfolio/frontend
+    build:
+      context: .
+      dockerfile: ./Dockerfile
     ports:
-      - "${SERVER_IP}:3000:3000"
+      - "${SERVER_PORT}:3000:3000"
+    restart: unless-stopped
+    networks:
+      - portfolio_net
 EOF
