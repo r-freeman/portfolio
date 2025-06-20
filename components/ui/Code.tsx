efd8dfb8f8e992335ccae728be5967070f1ccb00
Ryan Freeman <hello@ryanfreeman.dev>
2025-06-20 20:38:00 +0100

    Update code component

--- components/ui/Code.tsx
+++ components/ui/Code.tsx
@@ -19,9 +19,9 @@ export function Code({children}: { children: ReactNode }) {
     return (
         <pre className="relative group" ref={preRef}>
                 <button
-                    className="absolute top-0 right-0 m-5 text-zinc-400"
+                    className="absolute top-0 right-0 m-5 text-zinc-400 rounded-lg hover:bg-[#121212] p-2"
                     onClick={handleCopy} aria-label={`${!copied ? 'Copy this code' : 'Copied!'}`}>
-                    <div className="relative size-6">
+                    <div className="relative size-5">
                         <CheckIcon
                             className={clsx('absolute text-green-500 ease-in transform transition', !copied ? 'scale-0' : 'scale-100')}/>
                         <CopyIcon
